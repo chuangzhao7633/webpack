e916ad656303521ca59a62146e90bb85015214d9
chuangzhao <chuang.zhao666@foxmail.com>
2022-10-27 22:56:09 +0800

    webpack5 生产环境 css兼容性处理、压缩及优化配置

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,6 +1,26 @@
 const path = require('path');
 const EslintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+// 获取处理样式的 loader
+const getStyleLoader = () => [
+  // 提取 css 成单独文件，HtmlWebpackPlugin 插件会在 html 文件中自动 link 引入样式表
+  MiniCssExtractPlugin.loader,
+  'css-loader',
+  {
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
+        plugins: [
+          // 解决样式兼容性问题，根据 package.json 中 browserslist 配置的规则进行编译
+          'postcss-preset-env'
+        ]
+      }
+    }
+  }
+];
 
 module.exports = {
   entry: './src/main.js',
@@ -15,18 +35,11 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: getStyleLoader()
       },
       {
         test: /\.less$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'less-loader'
-        ]
+        use: [...getStyleLoader(), 'less-loader']
       }, 
       {
         test: /\.(png|jpe?g|gif|webp|svg)$/,
@@ -61,8 +74,18 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../public/index.html')
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'static/css/main.css'
+    }),
+    /* 
+      该插件使用 nanocss 压缩和优化 css
+      webpack4 中使用 optimize-css-assets-webpack-plugin 插件压缩代码
+      css-minimizer-webpack-plugin 在 source maps 和 assets 中使用查询字符串会更加准确，支持缓存和并发模式下运行
+    */
+    new CssMinimizerPlugin()
   ],
 
+  // 生产环境开启: 默认开启 html 和 js 压缩，所以只需要单独配置 css 压缩
   mode: 'production'
 }
