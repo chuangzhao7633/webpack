d791de7682878d31a37a3b0564f1aae54ebb4894
chuangzhao <chuang.zhao666@foxmail.com>
2022-10-29 17:31:51 +0800

    webpack5 优化配置 js兼容性彻底解决 core-js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/* 
+  babel 解析 es6 语法，但是如 promise async 以及后续的数组的 includes 等方法不会解析
+  为了彻底解决 js 兼容性问题，需要使用 core-js 这个库(对这些高级语法的底层实现进行引入)
+
+  完整引入: import 'core-js';
+  按需引入: import 'core-js/es/promise';
+  开发过程中自己按需引入太麻烦，在 babel 配置中可以设置自动按需引入
+    [
+      "@babel/preset-env",
+      {
+        "useBuiltIns": "usage", // 按需加载 自动引入
+        "corejs": "3.26.0" // corejs 版本
+      }
+    ]
+*/
 import count from "./js/count";
 /* import sum from "./js/sum"; */
 import './css/index.css';
